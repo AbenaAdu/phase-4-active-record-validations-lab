9f98da240e8c435a0ef49bd426f81901e0015ba8
Abena Adu <abeanadu@gmail.com>
2022-12-19 02:15:45 -0500

    commit tree 731e69809277906bc4619a0e466b76d706b5c611

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction" or "Non-fiction"]}
+    validate :clickbait
+
+
+    def clickbait
+
+        errrors.add(:title, "must be clickbait")
+    end
 end
